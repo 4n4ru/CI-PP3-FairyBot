18ed6e2adc5d1cbbd829311f584f2023bd096bde
Ana <ana.runje@live.com>
2021-07-17 15:54:37 +0000

    Feat: Add a class for child info that creates a dictionary

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
+class ChildInfo:
+    """
+    Child info class
+    """
+
+    def __init__(self, name, parent, friend, hair, eyes):
+        # instance attribute
+        self.name = name
+        self.parent = parent
+        self.friend = friend
+        self.hair = hair
+        self.eyes = eyes
+
+    def make_dictionary(self):
+        """
+        Creates a dictionary with child info
+        returns: dictionary
+        """
+        child_dict = {
+            'name': self.name,
+            'parent': self.parent,
+            'friend': self.friend,
+            'hair': self.hair,
+            'eyes': self.eyes
+        }
+        return child_dict
+
+
 def main():
     print('Welcome to Fairy Bot!\n')
 

478660826429d97bc08214c52567b4e1b715165e
Ana <ana.runje@live.com>
2021-07-17 16:24:22 +0000

    Feat: Add function to pick eye colour

--- run.py
+++ run.py
@@ -26,22 +26,46 @@ class ChildInfo:
         return child_dict
 
 
-def add_child():
+def pick_eye_color():
     """
-    Gets user input for child info
-    returns child
+    Gets user input for eye color
+    returns string
     """
-    name = input('Please enter your childs name:\n')
-    parent = input('Please enter the childs parents name:\n')
-    friend = input('Please enter your childs best friends name:\n')
-    hair = pick_hair_color()
-    eyes = pick_eye_color()
-    child = ChildInfo.child_dict(name, parent, friend, hair, eyes)
-    return child
+    print('Please choose your childs eye colour:')
+    print('1. Black')
+    print('2. Brown')
+    print('3. Green')
+    print('4. Blue')
+    eye_color = input()
+    if eye_color == 1:
+        return 'black'
+    elif eye_color == 2:
+        return 'brown'
+    elif eye_color == 3:
+        return 'green'
+    else:
+        return 'blue'
+
+
+# def add_child():
+#     """
+#     Gets user input for child info
+#     returns child
+#     """
+#     name = input('Please enter your childs name:\n')
+#     parent = input('Please enter the childs parents name:\n')
+#     friend = input('Please enter your childs best friends name:\n')
+#     hair = pick_hair_color()
+#     eyes = pick_eye_color()
+#     child = ChildInfo.child_dict(name, parent, friend, hair, eyes)
+#     return child
 
 
 def main():
     print('Welcome to Fairy Bot!\n')
-    child = add_child()
+    # child = add_child()
+    eye_color = pick_eye_color()
+    print(eye_color)
+
 
 main()

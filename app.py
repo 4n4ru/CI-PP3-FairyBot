c6c5c0a9b5109495e9a7abaac6e9991d77a15e24
Ana <ana.runje@live.com>
2021-07-24 22:15:20 +0000

    Refact: Wrap imports into try except blocks

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 def main():
-    import welcome
+    try:
+        import welcome
+    except ImportError:
+        raise ImportError('welcome module failed to import')
 
-    import story
+    try:
+        import story
+    except ImportError:
+        raise ImportError('story module failed to import')
     
-    import end
+    try:
+        import end
+    except ImportError:
+        raise ImportError('end module failed to import')
 
 if __name__ == "__main__":
     main()

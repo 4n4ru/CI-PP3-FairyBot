ccf16f3f95d98ca7fe826c64084df9e8d64b1f3d
Ana <ana.runje@live.com>
2021-07-25 03:09:59 +0000

    Maint: Add docstrings to save_story.py and define return types

--- save_story.py
+++ save_story.py
@@ -3,14 +3,29 @@ import prompts
 import shortuuid
 import csv
 
-def save_story_option():
+def save_story_option() -> bool:
+    """Asks the user if the last generated story should be saved for later
+
+    Returns:
+        bool: true if the story should be saved, false otherwise
+    """
     save = user_input.select_input(prompts.SAVE_OPTION, prompts.YES_NO_OPTIONS)
     if save.upper() == 'YES':
         return True
     else:
         return False
 
-def save_story(story):
+def save_story(story) -> str:
+    """Saves story to csv file.
+    Generates a user_id, creates a list with user_id and story as elements and
+    appends the list to the next row in the csv file
+
+    Args:
+        story (str): The last story generated passed as an argument
+
+    Returns:
+        str: user id
+    """
     user_id = shortuuid.uuid()[:7]
     row = [user_id, story]
     with open('stories/saved-stories.csv', 'a') as f:

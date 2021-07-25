0cf960cad39b313c3b18a01a2492052d2a0785e0
Ana <ana.runje@live.com>
2021-07-25 03:36:13 +0000

    Maint: add a docstring to welcome.py

--- welcome.py
+++ welcome.py
@@ -2,8 +2,9 @@ import prompts
 import config
 
 def welcome():
+    """Prints a welcome message and some ASCII art to the terminal
+    """
     print(prompts.WELCOME)
     print(config.ascii_book)
-    print('\n')
 
 welcome()

19762af5803f8f952efd203612d4968b1baab590
Ana <ana.runje@live.com>
2021-07-25 02:47:08 +0000

    Maint: Add new line in front of prompts for readability in terminal

--- prompts.py
+++ prompts.py
@@ -1,28 +1,29 @@
-ENTER_NAME = 'Please enter your childs name: \n'
-ENTER_FRIEND = 'Please enter your childs best friends name: \n'
-ENTER_COLOR = 'Please choose your childs favorite colour:'
+ENTER_NAME = '\nPlease enter your childs name: \n'
+ENTER_FRIEND = '\nPlease enter your childs best friends name: \n'
+ENTER_COLOR = '\nPlease choose your childs favorite colour:'
 COLORS = ['blue', 'pink', 'yellow', 'orange']
-ENTER_FOOD = 'Please enter your childs favourite food: \n'
-ENTER_ANIMAL = 'Please enter your childs favourite animal: \n'
-ENTER_SPORT = 'Please enter your childs favourite team sport: \n'
-ENTER_DISLIKED_FOOD = 'Please enter a food your child does not like: \n'
-ENTER_SEX = 'Is your child male or female? Please enter f\\m: \n'
+ENTER_FOOD = '\nPlease enter your childs favourite food: \n'
+ENTER_ANIMAL = '\nPlease enter your childs favourite animal: \n'
+ENTER_SPORT = '\nPlease enter your childs favourite team sport: \n'
+ENTER_DISLIKED_FOOD = '\nPlease enter a food your child does not like: \n'
+ENTER_SEX = '\nIs your child male or female? Please enter f\\m: \n'
 SEX_OPTIONS = ('F', 'f', 'M', 'm')
-CHOOSE_CHILD = 'Please choose the child the story should be personalized for: '
-SELECT_NUMBER = 'Please select a number: \n'
-NEW_STORY = 'Would you like to make a new story? Please enter yes\\no: \n'
+CHOOSE_CHILD = '\nPlease choose the child the story should\
+ be personalized for: '
+SELECT_NUMBER = '\nPlease select a number: \n'
+NEW_STORY = '\nWould you like to make a new story? Please enter yes\\no: \n'
 YES_NO_OPTIONS = ('YES', 'Yes', 'yes', 'NO', 'No', 'no')
-DELETE = 'Do you wish to delete your childs data? Please enter yes\\no: \n'
-DELETE_CHILD = 'Please choose the child you wish to delete: '
+DELETE = '\nDo you wish to delete your childs data? Please enter yes\\no: \n'
+DELETE_CHILD = '\nPlease choose the child you wish to delete: '
 WELCOME = 'Welcome to Fairy Bot!\n'
-NO_DELETE = 'There are no more children to delete.'
-THANK_YOU = 'Thank you for using the Fairy Bot app. If you want to restart it,\
- press the Run Fairy Bot button.'
-CHOOSE_STORY = 'The following stories are available: '
-SAVE_OPTION = 'Do you wish to save the story for later?\
+NO_DELETE = '\nThere are no more children to delete.'
+THANK_YOU = '\nThank you for using the Fairy Bot app.\
+ If you want to restart it, press the Run Fairy Bot button.'
+CHOOSE_STORY = '\nThe following stories are available: '
+SAVE_OPTION = '\nDo you wish to save the story for later?\
  Please enter yes\\no: \n'
-SUCCESS_SAVE = 'The story was successfuly saved.\
+SUCCESS_SAVE = '\nThe story was successfuly saved.\
  Please note your user id to access it later: '
 RETRIEVE_OPTION = 'Do you wish to retrieve a saved story?\
  Please enter yes\\no: \n'
-ENTER_USER_ID = 'Please enter your user id: \n'
+ENTER_USER_ID = '\nPlease enter your user id: \n'
